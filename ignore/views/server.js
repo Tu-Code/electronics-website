6f53d7c3b5037ff1ad0a477a067c7bacbf2ad355
Sarah Solarin <solarinsarah3@gmail.com>
2023-10-22 10:56:21 +0100

    First commit

---
+++ ignore/views/server.js
@@ -0,0 +1,25 @@
+// const express = require("express"),
+//   app = express();
+
+// //setting view engine to ejs
+// app.set("view engine", "ejs");
+// app.use(express.static(__dirname + '/'));
+
+// //route for index page
+// app.get("/", function (req, res) {
+//   res.render("pages/index");
+// });
+
+// //route for magic page
+// app.get("/contact", function (req, res) {
+//   res.render("pages/contact");
+// });
+
+// //route for magic page
+// app.get("/about", function (req, res) {
+//     res.render("pages/about");
+//   });
+
+// app.listen(8080, function () {
+//   console.log("Server is running on port 8080 ");
+// });
